21516396e8877132ae4dfc420cebc578b4c395b5
Brandon T <bthomas@brave.com>
2019-08-15 10:57:46 -0400

    Removed unnecessary code.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -3062,11 +3062,6 @@ extension BrowserViewController: PreferencesObserver {
             updateTabsBarVisibility()
             updateApplicationShortcuts()
             
-            // Destroy all tab data
-            tabManager.allTabs.forEach({
-                tabManager.removeAllBrowsingDataForTab($0)
-            })
-            
             tabManager.removeAll()
             
             // Clear all persistent data
